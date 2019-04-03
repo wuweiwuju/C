c0a0bcd96165c49852484df5d3885f852a7ca4d5
李鹏飞 <305086028@qq.com>
2019-04-03 23:32:22 +0800

    上传代码

---
+++ test_4_2_4/test.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int a, b, c;
+	int t = 0;
+	scanf("%d %d %d", &a, &b, &c);
+	if (a < b)
+	{
+		t = a;
+		a = b;
+		b = t;
+	}
+	if (a < c);
+	{
+		t = a;
+		a = c;
+		c = t;
+	}
+	if (b < c)
+	{
+		t = b;
+		b = c;
+		c = t;
+	}
+	printf("这三个数由大到小为%d %d %d\n", a, b, c);
+	system("pause");
+	return 0;
+}
