3049f2997d9654e22988f2e11c22c9606e6bb133
李鹏飞 <305086028@qq.com>
2019-08-07 23:46:40 +0800

    上传代码

---
+++ C调试/test.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	char* p = "hello bit.";
+//	printf("%s\n", p);
+//	return 0;
+//
+//}
+#include <stdio.h>
+int main()
+{
+	int i = 0;
+	int arr[10] = { 1,2,4,5,6,8,75,5,3,6 };
+	/*printf("%p\n", &i);
+	printf("%p\n", arr);*/
+	for (i = 0; i <= 12; i++)
+	{
+		arr[i] = 0;
+		printf("hehe\n");
+	}
+	return 0;
+}
